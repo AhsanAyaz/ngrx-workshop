d4584d48f4ae1b6b43397e5f0a435314bfff2cbc
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-03-16 01:15:25 +0100

    promisify github action steps

--- .github/actions/angular-magic/action.js
+++ .github/actions/angular-magic/action.js
@@ -3,23 +3,41 @@ const github = require("@actions/github");
 const run = require("github-pages-deploy-action").default;
 
 const execute = (command) => {
-  const exec = require("child_process").exec;
-  exec(command, (err, stdout, stderr) => {
-    process.stdout.write(stdout);
-  });
+  return new Promise((resolve, reject) => {
+    const exec = require("child_process").exec;
+    exec(command, (err, stdout, stderr) => {
+      if (err) {
+        return reject(err);
+      }
+      process.stdout.write(stdout);
+      resolve();
+    });
+  })
 }
 console.log('starting process');
 const buildConfig = core.getInput("build_configuration");
 const baseHref = core.getInput("base_href");
 const githubAccessToken = core.getInput("github_access_token");
 const deployFolder = core.getInput("deploy_folder");
-execute("npm install -g @angular/cli");
 execute("npm install")
-execute("ng lint")
-execute(`ng build --configuration=${buildConfig} --base-href=${baseHref}`)
-run({
-  accessToken: githubAccessToken,
-  branch: "gh-pages",
-  folder: deployFolder,
-});
-console.log('process ended');
+  .then(() => {
+    return execute("node_modules/.bin/ng lint");
+  })
+  .then(() => {
+    return execute(`node_modules/.bin/ng build --configuration=${buildConfig} --base-href=${baseHref}`);
+  })
+  .then(() => {
+    return run({
+      accessToken: githubAccessToken,
+      branch: "gh-pages",
+      folder: deployFolder,
+    });
+  })
+  .then(() => {
+    console.log('process ended');
+  })
+  .catch(err => {
+    console.log(err);
+    core.setFailed(err);
+  });
+
