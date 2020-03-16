0b89d406399c209c864f0a6c752fc4351fbf1ee1
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-03-16 01:41:33 +0100

    testing with run_lint=false

--- .github/actions/angular-magic/action.js
+++ .github/actions/angular-magic/action.js
@@ -7,7 +7,7 @@ const baseHref = core.getInput("base_href");
 const githubAccessToken = core.getInput("github_access_token");
 const deployFolder = core.getInput("deploy_folder");
 const runLint = core.getInput("run_lint");
-console.log('runLint value = ', runLint);
+console.log('runLint value =', runLint);
 console.log(typeof runLint);
 console.log(runLint === 'false')
 const main = async () => {
